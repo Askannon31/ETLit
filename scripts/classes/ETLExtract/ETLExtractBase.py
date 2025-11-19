519a5a55d341e32e5604938d8b574de31d766f33
Tobias Liese <tobias.liese@gws.ms>
2025-11-19 10:55:38 +0100

    Enhance ETLExtractBase with debug data saving functionality and import statements

--- scripts/classes/ETLExtract/ETLExtractBase.py
+++ scripts/classes/ETLExtract/ETLExtractBase.py
@@ -1,6 +1,21 @@
 ########################################################################################################################
 # Base class for ETL Extract                                                                                           #         
 ########################################################################################################################
+import datetime
+import json
+import logging
+import requests
+
+
+########################################################################################################################
+#                                                          Setup                                                       #
+########################################################################################################################
+# Setup Logger
+log = logging.getLogger(__name__)
+
+########################################################################################################################
+# ETLExtractBase                                                                                                       #         
+########################################################################################################################
 class ETLExtractBase:
     def __init__(self, config):
         self.config = config
@@ -13,3 +28,25 @@ class ETLExtractBase:
 
     def extract(self) -> dict:
         raise NotImplementedError("Extract method must be implemented by subclasses.")
+    
+    def save_debug_data(self, data: dict):
+        """
+        Saves the extracted data to a debug file
+        """
+        import json
+        now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        debug_file: str = f"debug/{self.name}_{now}_debug_data.json"
+        with open(debug_file, "w") as f:
+            json.dump(data, f, indent=4)
+        log.debug(f"Saved debug data to {debug_file}")
+    
+    def save_debug_data_mapped(self, data: dict):
+        """
+        Saves the mapped data to a debug file
+        """
+        import json
+        now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        debug_file: str = f"debug/{self.name}_{now}_debug_mapped_data.json"
+        with open(debug_file, "w") as f:
+            json.dump(data, f, indent=4)
+        log.debug(f"Saved debug data to {debug_file}")
